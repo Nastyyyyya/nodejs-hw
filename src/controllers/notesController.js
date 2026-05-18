3a3f198870180032ff33e3d4f5fff97c3e21adee
Anastasia Paslavska <a.paslavska2206@gmail.com>
2026-05-19 01:28:17 +0300

    add changes from 2 homework

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -96,8 +96,9 @@ export const deleteNote = async (req, res, next) => {
 export const updateNote = async (req, res, next) => {
   try {
     const { noteId } = req.params;
+
     const note = await Note.findByIdAndUpdate(noteId, req.body, {
-      new: true,
+      returnDocument: 'after',
       runValidators: true,
     });
 
